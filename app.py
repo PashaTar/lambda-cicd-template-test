07790db20255ae4b40598140bf22c02459a81422
Pavel Tarasenko <pavel.tarasenko.au@gmail.com>
2023-12-15 10:57:08 +1000

    updated buildspec file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # https://www.youtube.com/watch?v=3CcGtRidF9c
 # https://www.youtube.com/watch?v=6SSLBfOZOtQ
+# https://www.youtube.com/watch?v=C99ps_cWp9Y&t=132s ++
 
 import pandas as pd
 import json
